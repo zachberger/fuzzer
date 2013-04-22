79712b223a68f51fa60e87ca4757cce3d95d7233
cjpresler <c.j.presler@gmail.com>
2013-04-22 18:14:02 -0400

    Added functionality on PageInfo to discover inputs.

--- src/edu/rit/se/security/fuzzer/Fuzzer.java
+++ src/edu/rit/se/security/fuzzer/Fuzzer.java
@@ -1,12 +1,28 @@
 package edu.rit.se.security.fuzzer;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+
 public class Fuzzer {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		PageInfo pageInfo = new PageInfo();
+		try {
+			pageInfo.rootURL = new URL("http://www.w3schools.com/html/html_forms.asp");
+			pageInfo.discoverInputs();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+		}
 	}
 
 }
--- src/edu/rit/se/security/fuzzer/PageInfo.java
+++ src/edu/rit/se/security/fuzzer/PageInfo.java
@@ -1,10 +1,19 @@
 package edu.rit.se.security.fuzzer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomAttr;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class PageInfo {
 
@@ -39,6 +48,32 @@ public class PageInfo {
 		String origQuery = rootURL.getQuery() == null ? "" : rootURL.getQuery();
 		String origNoQuery = rootURL.toString().replace("?"+origQuery, "");
 		return origNoQuery.hashCode();
+	}
+	
+	/**
+	 * Discovers all inputs of the page and stores them in supportedActions
+	 * 
+	 * @throws FailingHttpStatusCodeException
+	 * @throws IOException
+	 */
+	public void discoverInputs() throws FailingHttpStatusCodeException, IOException{
+		WebClient webClient = new WebClient();
+		HtmlPage page = webClient.getPage(rootURL);
+		for(HtmlForm form : page.getForms()){
+			Set<String> methodInputs = null;
+			switch(form.getMethodAttribute().toLowerCase()){
+				case "post": methodInputs = supportedActions.get(HTTPMethod.POST); break;
+				case "get": methodInputs = supportedActions.get(HTTPMethod.GET); break;
+				case "put": methodInputs = supportedActions.get(HTTPMethod.PUT); break;
+				case "delete": methodInputs = supportedActions.get(HTTPMethod.DELETE); break;
+				default: methodInputs = supportedActions.get(HTTPMethod.GET); // No form method was specified, defaults to GET
+			}
+			if(methodInputs != null){ // Ensure that the form is set
+				for(DomAttr input : (List<DomAttr>)form.getByXPath("//input/@id")){
+					methodInputs.add(input.getValue());
+				}
+			}
+		}
 	}
 	
 	public String toString(){

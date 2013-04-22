c29f96c39dc32ecc4e25cc1737aabde5f17ba35b
Zach Berger <zsb1244@rit.edu>
2013-04-22 10:01:58 -0400

    Renamed page to avoid conflicts with HTMLUnit's Page class

---
+++ src/edu/rit/se/security/fuzzer/PageInfo.java
@@ -0,0 +1,48 @@
+package edu.rit.se.security.fuzzer;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PageInfo {
+
+	URL rootURL;
+	Map<HTTPMethod,Set<String>> supportedActions;
+	
+	public PageInfo(){
+		supportedActions = new HashMap<HTTPMethod,Set<String>>();
+		supportedActions.put(HTTPMethod.GET, new HashSet<String>());	
+		supportedActions.put(HTTPMethod.PUT, new HashSet<String>());	
+		supportedActions.put(HTTPMethod.POST, new HashSet<String>());	
+		supportedActions.put(HTTPMethod.DELETE, new HashSet<String>());		
+	}
+	
+	@Override
+	public boolean equals( Object o ){
+		if( o instanceof PageInfo ){
+			PageInfo castedObject = (PageInfo) o;
+			if( castedObject.rootURL != null ){
+				String origQuery = rootURL.getQuery() == null ? "" : rootURL.getQuery();
+				String origNoQuery = rootURL.toString().replace("?"+origQuery, "");
+				String castedQuery = castedObject.rootURL.getQuery() == null ? "" : castedObject.rootURL.getQuery();
+				String castedNoQuery = castedObject.rootURL.toString().replace("?"+castedQuery,"");
+				//System.out.println(origNoQuery + ":" +castedNoQuery);
+				return origNoQuery.equals(castedNoQuery);
+			}
+		}
+		return false;
+	}
+	
+	public int hashCode(){
+		String origQuery = rootURL.getQuery() == null ? "" : rootURL.getQuery();
+		String origNoQuery = rootURL.toString().replace("?"+origQuery, "");
+		return origNoQuery.hashCode();
+	}
+	
+	public String toString(){
+		return rootURL.toString();
+	}
+	
+}

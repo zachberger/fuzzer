aab7e5df56ee3a46a973fd52d1ff3202c55988bd
Zach Berger <zsb1244@rit.edu>
2013-04-28 15:25:29 -0400

    Now logs in to the DVWA app

--- src/edu/rit/se/security/fuzzer/DVWAPageEnumerator.java
+++ src/edu/rit/se/security/fuzzer/DVWAPageEnumerator.java
@@ -1,6 +1,13 @@
 package edu.rit.se.security.fuzzer;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
 public class DVWAPageEnumerator extends PageEnumerator {
 
@@ -10,7 +17,22 @@ public class DVWAPageEnumerator extends PageEnumerator {
 
 	@Override
 	public void beforeStart(){
-		//login
+		try {
+			HtmlPage loginPage = wc.getPage(rootURL);
+			HtmlForm loginForm = loginPage.getForms().get(0);
+			loginForm.getInputByName("username").setValueAttribute("admin");
+			loginForm.getInputByName("password").setValueAttribute("password");
+			HtmlSubmitInput submit = loginForm.getInputByName("Login");
+			HtmlPage dvwaPage = submit.click();
+			rootURL = dvwaPage.getUrl();
+		} catch (FailingHttpStatusCodeException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) throws MalformedURLException{
+		PageEnumerator p = new DVWAPageEnumerator(new URL("http://127.0.0.1/dvwa/login.php"));
+		p.start();
+	}
 }
